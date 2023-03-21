31d1f4087c53fc26ce7239283c6f31d120e7b574
Vinícius Marinho <vinicius.marinho@kbrtec.com.br>
2023-03-21 01:37:30 -0300

    Adding files

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: false,
+  treilingComa: 'all',
+  arrowParams: 'avoid',
+  endOfLine: 'auto',
+};
---
+++ src/index.js
@@ -0,0 +1,56 @@
+const { response } = require("express");
+const express = require("express");
+const { u4: uuidv4 } = require("uuid");
+
+const app = express();
+
+const customers = [];
+
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customer.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+  });
+});
+
+// app.use(verifyIfExistsAccountCPF);
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer.statement);
+});
+
+app.listen(3333);
